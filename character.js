c09639cbe5e4585366eae1bfb04f886b9641447d
Alice Smith <alice@webage.com>
2025-03-04 10:19:36 -0500

    Added Vehicle to Character

--- character.js
+++ character.js
@@ -4,6 +4,7 @@ let heightSpan;
 let massSpan;
 let filmsDiv;
 let planetDiv;
+let vehicleSpan;
 const baseUrl = `http://localhost:9001/api`;
 
 // Runs on page load
@@ -14,6 +15,7 @@ addEventListener('DOMContentLoaded', () => {
   populationSpan = document.querySelector('span#height');
   homeworldSpan = document.querySelector('span#homeworld');
   filmsUl = document.querySelector('#films>ul');
+  vehicleSpan = document.querySelector('span#vehicle_class');
   const sp = new URLSearchParams(window.location.search)
   const id = sp.get('id')
   getCharacter(id)
@@ -25,6 +27,7 @@ async function getCharacter(id) {
     character = await fetchCharacter(id)
     character.homeworld = await fetchHomeworld(character)
     character.films = await fetchFilms(character)
+    character.vehicle = await fetchVehicle(character);
   }
   catch (ex) {
     console.error(`Error reading character ${id} data.`, ex.message);
@@ -52,6 +55,15 @@ async function fetchFilms(character) {
   return films;
 }
 
+async function fetchVehicle(character) {
+    const url = `${baseUrl}/vehicles/${character?.id}`;
+    const response = await fetch(url)
+    if (!response.ok) return null
+    const vehicle = await response.json();
+    return vehicle.vehicle_class;
+  
+}
+
 const renderCharacter = character => {
   document.title = `SWAPI - ${character?.name}`;  // Just to make the browser tab say their name
   nameH1.textContent = character?.name;
@@ -61,4 +73,11 @@ const renderCharacter = character => {
   homeworldSpan.innerHTML = `<a href="/planet.html?id=${character?.homeworld.id}">${character?.homeworld.name}</a>`;
   const filmsLis = character?.films?.map(film => `<li><a href="/film.html?id=${film.id}">${film.title}</li>`)
   filmsUl.innerHTML = filmsLis.join("");
-}
+
+if (character?.vehicle) {
+  vehicleSpan.innerHTML = `<a href="/vehicle.html?id=${character?.id}">${character?.vehicle}</a>`;
+} else {
+  vehicleSpan.textContent = "No vehicle assigned";
+} 
+};
+
